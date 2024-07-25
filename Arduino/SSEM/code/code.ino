e8f110c79b103ded8feb7df96ffe0943d758da4b
Gerardo Guzman <guzman.tello.gerardo.josue@gmail.com>
2024-07-25 15:53:47 -0600

    actualizado

--- Arduino/SSEM/code/code.ino
+++ Arduino/SSEM/code/code.ino
@@ -1,3 +1,13 @@
 //Codigo para la placa SSEM
 #include <espnow.h>
 #include <ESP8266WiFi.h>
+
+
+void setup(){
+    Serial.begin(9600);
+}
+
+void loop(){
+    Serial.println("Hola mundo");
+    delay(600);
+}
